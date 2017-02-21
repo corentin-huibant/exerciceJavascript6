b59c8848060aabf46b6f09c56399a4f51efb4139
Corentin <corentin.huibant@laposte.net>
2017-02-21 19:44:30 +0100

    Demander à l’utilisateur deux nombres à virgule. Ne garder que la partie entière du premier. Les multiplier et afficher le résultat dans une boîte de dialogue.

--- modulo.js
+++ modulo.js
@@ -1,14 +1,13 @@
-// fonction permettant de faire le modulo de deux chiffres
 function number() {
   var number1 = document.getElementById('premier_nombre').value;
   var number2 = document.getElementById('deuxieme_nombre').value;
     if (isNaN(number1 && number2) == true) {
       alert('Vous avez fait une erreur !');
-    }
+    } // condition qui empeche le client d'inscrire une valeur autre qu'une nombre.
     else if (number2 == 0) {
       alert('Vous ne pouvez pas diviser par zéro !');
-    }
+    } //condition qui empeche le client d'inscrire 0 comme diviseur.
     else {
   alert(number1 % number2);
-  }
+} // Condition permettant de faire le modulo de deux chiffres.
 }
